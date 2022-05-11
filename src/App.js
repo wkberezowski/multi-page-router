fe387475225d389561b68a4e1e1b3ecb68b7be05
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-11 09:54:32 +0200

    added swtich and exact

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
 import Products from './pages/Products';
 import Welcome from './pages/Welcome';
 
 function App() {
   return (
     <div>
-      <Route path='/welcome'>
-        <Welcome />
-      </Route>
-      <Route path='/products'>
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path='/welcome'>
+            <Welcome />
+          </Route>
+          <Route path='/products' exact>
+            <Products />
+          </Route>
+          <Route path='/products/:productId'>
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
